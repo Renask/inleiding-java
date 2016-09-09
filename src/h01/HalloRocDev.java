457b6c3020405ebd1b470a881dfae2072397a915
renas <rkhalil@roc-dev.com>
2016-09-09 09:30:25 +0200

    Hoofdstuk 1 T/M 3

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -7,6 +7,6 @@ public class HalloRocDev {
 
     public static void main(String[] args)
     {
-        System.out.println("Good Morning!");
+        System.out.println("Good Night!");
     }
 }
---
+++ src/h01/Main.java
@@ -0,0 +1,17 @@
+package h01;
+
+
+import java.awt.*;
+import java.applet.*;
+
+public class Main extends Applet {
+
+    public void init() {
+        setBackground(Color.magenta);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+}
