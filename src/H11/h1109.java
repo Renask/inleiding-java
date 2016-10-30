5cb386e893c5b2c9b1e1f5fedf46b3ba41ef0109
renas <rkhalil@roc-dev.com>
2016-10-30 16:52:23 +0100

    Huiswerk

--- src/H11/h1109.java
+++ src/H11/h1109.java
@@ -1,42 +1,35 @@
 package H11;
-
 import java.awt.*;
 import java.applet.*;
-
-/**
- * Created by renas on 28-10-2016.
- */
-
-
-public class h1109{
+public class h1109 extends Applet {
     public void paint(Graphics g) {
-       int y = 50;
-       int x = 50;
-        for (int kolom = 0; kolom < 8; kolom++) {
-
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, 50, 50);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, 50, 50);
+        g.drawRect(49, 49, 81, 81);
+        int y = 50;
+        for (int i = 0; i < 4; i++) {
+            int x = 50;
+            for (int kolom = 0; kolom < 8; kolom++) {
+                if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                    g.setColor(Color.black);
+                    g.fillRect(x, y, 10, 10);
+                } else {
+                    g.setColor(Color.white);
+                    g.fillRect(x, y, 10, 10);
+                }
+                x += 10;
             }
-            x += 50;
-        }
-
-        //tweede rij
-        x = 50;
-        y += 50;
-
-        for (int kolom = 0; kolom < 8; kolom++) {
-            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
-                g.setColor(Color.black);
-                g.fillRect(x, y, 50, 50);
-            } else {
-                g.setColor(Color.white);
-                g.fillRect(x, y, 50, 50);
-
+            y += 10;
+            x = 50;
+            for (int kolom = 0; kolom < 8; kolom++) {
+                if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                    g.setColor(Color.white);
+                    g.fillRect(x, y, 10, 10);
+                } else {
+                    g.setColor(Color.black);
+                    g.fillRect(x, y, 10, 10);
+                }
+                x += 10;
             }
-            x += 50;
+            y += 10;
         }
-    }}
+    }
+}
